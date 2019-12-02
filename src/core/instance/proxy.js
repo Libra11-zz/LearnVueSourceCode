5400c6048ab01c3861868ac782d407c2a9dd4ac7
Libra <libra_085925@163.com>
2019-12-02 19:34:45 +0800

    initProxy

--- src/core/instance/proxy.js
+++ src/core/instance/proxy.js
@@ -7,6 +7,9 @@ let initProxy
 
 // 在生产环境下我们导出的 initProxy 实际上就是 undefined。只有在非生产环境下导出的 initProxy 才会有值
 if (process.env.NODE_ENV !== 'production') {
+  // 可以看到 allowedGlobals 实际上是通过 makeMap 生成的函数，
+  // 所以 allowedGlobals 函数的作用是判断给定的 key 是否出现在上面字符串中定义的关键字中的。这些关键字都是在 js 中可以全局访问的
+  // 下面这些全局变量都是可以在{{}}中访问的
   const allowedGlobals = makeMap(
     'Infinity,undefined,NaN,isFinite,isNaN,' +
     'parseFloat,parseInt,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent,' +
@@ -55,10 +58,19 @@ if (process.env.NODE_ENV !== 'production') {
   }
 
   const hasHandler = {
+    /**
+     * has 可以拦截以下操作：
+        属性查询: foo in proxy
+        继承属性查询: foo in Object.create(proxy)
+        with 检查: with(proxy) { (foo); }
+        Reflect.has()
+     */
     has(target, key) {
       const has = key in target
+      // 如果 key 在 allowedGlobals 之内，或者 key 是以下划线 _ 开头的字符串，则为真
       const isAllowed = allowedGlobals(key) ||
         (typeof key === 'string' && key.charAt(0) === '_' && !(key in target.$data))
+      // 如果 has 和 isAllowed 都为假，使用 warnNonPresent 函数打印错误
       if (!has && !isAllowed) {
         if (key in target.$data) warnReservedPrefix(target, key)
         else warnNonPresent(target, key)
@@ -85,9 +97,11 @@ if (process.env.NODE_ENV !== 'production') {
       // 如果 Proxy 存在，那么将会使用 Proxy 对 vm 做一层代理，代理对象赋值给 vm._renderProxy，
       // 所以今后对 vm._renderProxy 的访问，如果有代理那么就会被拦截。代理对象配置参数是 handlers，
       // 可以发现 handlers 既可能是 getHandler 又可能是 hasHandler，至于到底使用哪个，是由判断条件决定的
+      // 一般都使用 hasHandler 
       const handlers = options.render && options.render._withStripped
         ? getHandler
         : hasHandler
+      // vm._renderProxy 在 core/instance/render.js 文件中的 Vue.prototype._render 使用到了
       vm._renderProxy = new Proxy(vm, handlers)
     } else {
       vm._renderProxy = vm
